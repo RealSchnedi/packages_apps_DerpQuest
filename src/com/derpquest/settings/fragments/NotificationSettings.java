ffbfb3963a5f166524f16787af34b2e3517b8cce
maxwen <max.weninger@gmail.com>
2021-01-19 19:54:49 +0100

    DerpQuest: Add ambient pulse and aod notifications [2/2]
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>

--- src/com/derpquest/settings/fragments/NotificationSettings.java
+++ src/com/derpquest/settings/fragments/NotificationSettings.java
@@ -45,6 +45,9 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
+import com.derp.support.colorpicker.ColorPickerPreference;
+import com.derp.support.preference.CustomSystemSeekBarPreference;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -53,6 +56,15 @@ public class NotificationSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
+    private static final String NOTIFICATION_PULSE_COLOR = "ambient_notification_light_color";
+    private static final String AMBIENT_LIGHT_DURATION = "ambient_light_duration";
+    private static final String AMBIENT_LIGHT_REPEAT_COUNT = "ambient_light_repeat_count";
+    private static final String PULSE_COLOR_MODE_PREF = "ambient_notification_light_color_mode";
+
+    private ColorPickerPreference mEdgeLightColorPreference;
+    private CustomSystemSeekBarPreference mEdgeLightDurationPreference;
+    private CustomSystemSeekBarPreference mEdgeLightRepeatCountPreference;
+    private ListPreference mColorMode;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -65,10 +77,97 @@ public class NotificationSettings extends SettingsPreferenceFragment implements
         if (!Utils.isVoiceCapable(getActivity())) {
             prefScreen.removePreference(incallVibCategory);
         }
+
+        mEdgeLightRepeatCountPreference = (CustomSystemSeekBarPreference) findPreference(AMBIENT_LIGHT_REPEAT_COUNT);
+        mEdgeLightRepeatCountPreference.setOnPreferenceChangeListener(this);
+        int repeats = Settings.System.getInt(getContentResolver(),
+                Settings.System.AMBIENT_LIGHT_REPEAT_COUNT, 0);
+        mEdgeLightRepeatCountPreference.setValue(repeats);
+
+        mEdgeLightDurationPreference = (CustomSystemSeekBarPreference) findPreference(AMBIENT_LIGHT_DURATION);
+        mEdgeLightDurationPreference.setOnPreferenceChangeListener(this);
+        int duration = Settings.System.getInt(getContentResolver(),
+                Settings.System.AMBIENT_LIGHT_DURATION, 2);
+        mEdgeLightDurationPreference.setValue(duration);
+
+        mColorMode = (ListPreference) findPreference(PULSE_COLOR_MODE_PREF);
+        int value;
+        boolean colorModeAutomatic = Settings.System.getInt(getContentResolver(),
+                Settings.System.NOTIFICATION_PULSE_COLOR_AUTOMATIC, 0) != 0;
+        boolean colorModeAccent = Settings.System.getInt(getContentResolver(),
+                Settings.System.NOTIFICATION_PULSE_ACCENT, 0) != 0;
+        if (colorModeAutomatic) {
+            value = 0;
+        } else if (colorModeAccent) {
+            value = 1;
+        } else {
+            value = 2;
+        }
+
+        mColorMode.setValue(Integer.toString(value));
+        mColorMode.setSummary(mColorMode.getEntry());
+        mColorMode.setOnPreferenceChangeListener(this);
+
+        mEdgeLightColorPreference = (ColorPickerPreference) findPreference(NOTIFICATION_PULSE_COLOR);
+        int edgeLightColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.NOTIFICATION_PULSE_COLOR, 0xFF3980FF);
+        mEdgeLightColorPreference.setNewPreviewColor(edgeLightColor);
+        mEdgeLightColorPreference.setAlphaSliderEnabled(false);
+        String edgeLightColorHex = String.format("#%08x", (0xFF3980FF & edgeLightColor));
+        if (edgeLightColorHex.equals("#ff3980ff")) {
+            mEdgeLightColorPreference.setSummary(R.string.color_default);
+        } else {
+            mEdgeLightColorPreference.setSummary(edgeLightColorHex);
+        }
+        mEdgeLightColorPreference.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mEdgeLightColorPreference) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            if (hex.equals("#ff3980ff")) {
+                preference.setSummary(R.string.color_default);
+            } else {
+                preference.setSummary(hex);
+            }
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.NOTIFICATION_PULSE_COLOR, intHex);
+            return true;
+        } else if (preference == mEdgeLightRepeatCountPreference) {
+                int value = (Integer) newValue;
+                Settings.System.putInt(getContentResolver(),
+                        Settings.System.AMBIENT_LIGHT_REPEAT_COUNT, value);
+                return true;
+        } else if (preference == mEdgeLightDurationPreference) {
+            int value = (Integer) newValue;
+                Settings.System.putInt(getContentResolver(),
+                    Settings.System.AMBIENT_LIGHT_DURATION, value);
+            return true;
+        } else if (preference == mColorMode) {
+             int value = Integer.valueOf((String) newValue);
+            int index = mColorMode.findIndexOfValue((String) newValue);
+            mColorMode.setSummary(mColorMode.getEntries()[index]);
+            if (value == 0) {
+                Settings.System.putInt(getContentResolver(),
+                        Settings.System.NOTIFICATION_PULSE_COLOR_AUTOMATIC, 1);
+                Settings.System.putInt(getContentResolver(),
+                        Settings.System.NOTIFICATION_PULSE_ACCENT, 0);
+            } else if (value == 1) {
+                Settings.System.putInt(getContentResolver(),
+                        Settings.System.NOTIFICATION_PULSE_COLOR_AUTOMATIC, 0);
+                Settings.System.putInt(getContentResolver(),
+                        Settings.System.NOTIFICATION_PULSE_ACCENT, 1);
+            } else {
+                Settings.System.putInt(getContentResolver(),
+                        Settings.System.NOTIFICATION_PULSE_COLOR_AUTOMATIC, 0);
+                Settings.System.putInt(getContentResolver(),
+                        Settings.System.NOTIFICATION_PULSE_ACCENT, 0);
+            }
+            return true;
+        }
         return false;
     }
 
