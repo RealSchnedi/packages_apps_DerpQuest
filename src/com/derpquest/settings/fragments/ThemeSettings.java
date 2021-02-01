a2987de6ceed16d6484d55178f4daef684857759
RealSchnedi <schnedi3@gmail.com>
2021-02-01 17:50:30 +0100

    DerpQuest: Remove UI Styles picker

--- src/com/derpquest/settings/fragments/ThemeSettings.java
+++ src/com/derpquest/settings/fragments/ThemeSettings.java
@@ -77,12 +77,6 @@ public class ThemeSettings extends DashboardFragment {
         controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.accent_color"));
         controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.style.android"));
-        controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.style.settings"));
-        controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.style.systemui"));
-        controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.font"));
         controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.adaptive_icon_shape"));
